79b16f813d458952d50a6ae8477cb4ae7a893762
Jeo D <e8gea6w5z8id@opayq.com>
2019-01-23 16:31:45 -0700

    Add feature for random opening movie text

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      openingCrawl: ''
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://swapi.co/api/films/');
+    const films = await response.json();
+    const randomNum = Math.floor(Math.random() * films.count);
+    const openingCrawl = films.results[randomNum].opening_crawl;
+    this.setState({ openingCrawl });
+  }
+
   render() {
     return (
       <div className="App">
+        <p>{this.state.openingCrawl}</p>
       </div>
     );
   }
