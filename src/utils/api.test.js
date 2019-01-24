d5c4b701cd6abc292ada4ee5a4b3b2bfda23df96
Jeo D <e8gea6w5z8id@opayq.com>
2019-01-24 16:00:14 -0700

    Write tests for fetchOpeningCrawl in api helper file

--- src/utils/api.test.js
+++ src/utils/api.test.js
@@ -1,19 +1,31 @@
 import * as api from './api';
 import { shallow } from 'enzyme';
 
-describe('fetchOpeningCrawl', () => {
-  it('should call fetch with the correct params', async () => {
-    const mockFilms = {
-      count: 1,
-      results: [{ opening_crawl: 'in a galaxy far, far away...' }]
-    };
-    const expected = 'https://swapi.co/api/films/';
-    window.fetch = jest.fn().mockImplementation(() => {
-      return Promise.resolve({
-        json: () => Promise.resolve(mockFilms)
-      });
+describe('api', () => {
+  
+  describe('fetchOpeningCrawl', () => {
+    beforeEach(() => {
+      const mockFilms = {
+        count: 1,
+        results: [{ opening_crawl: 'in a galaxy far, far away...' }]
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+          return Promise.resolve({
+            json: () => Promise.resolve(mockFilms)
+          });
+        });
     });
-    await api.fetchOpeningCrawl();
-    expect(window.fetch).toHaveBeenCalledWith(expected);
+
+    it('should call fetch with the correct params', async () => {
+      const expected = 'https://swapi.co/api/films/';
+      await api.fetchOpeningCrawl();
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    });
+
+    it('should return a Promise that resolves to a string', async () => {
+      const expected = 'in a galaxy far, far away...';
+      const result = await api.fetchOpeningCrawl();
+      expect(result).toEqual(expected);
+    });
   });
-});
+})
