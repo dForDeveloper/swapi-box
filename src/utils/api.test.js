ad5dd0db27dcc9433b25c43039279c49b7858463
Jeo D <e8gea6w5z8id@opayq.com>
2019-01-24 16:34:46 -0700

    Write tests for fetchData in api helper file

--- src/utils/api.test.js
+++ src/utils/api.test.js
@@ -7,7 +7,9 @@ describe('api', () => {
     beforeEach(() => {
       const mockFilms = {
         count: 1,
-        results: [{ opening_crawl: 'in a galaxy far, far away...' }]
+        results: [{
+          opening_crawl: 'A long time ago in a galaxy far, far away...'
+        }]
       };
       window.fetch = jest.fn().mockImplementation(() => {
           return Promise.resolve({
@@ -16,15 +18,38 @@ describe('api', () => {
         });
     });
 
-    it('should call fetch with the correct params', async () => {
+    it('should call fetch with the correct parameter', async () => {
       const expected = 'https://swapi.co/api/films/';
       await api.fetchOpeningCrawl();
       expect(window.fetch).toHaveBeenCalledWith(expected);
     });
 
     it('should return a Promise that resolves to a string', async () => {
-      const expected = 'in a galaxy far, far away...';
+      const expected = 'A long time ago in a galaxy far, far away...';
       const result = await api.fetchOpeningCrawl();
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('fetchData', () => {
+    beforeEach(() => {
+      const mockData = { results: ['data'] }
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockData)
+        });
+      });
+    });
+
+    it('should call fetch with the correct parameter', () => {
+      const expected = 'https://swapi.co/api/people/'
+      api.fetchData('people');
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    });
+    
+    it('should return a promise that resolves to an object', async () => {
+      const expected = { people: ['data'] };
+      const result = await api.fetchData('people');
       expect(result).toEqual(expected);
     });
   });
