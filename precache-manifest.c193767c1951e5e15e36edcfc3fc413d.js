926f866837b88c5b2f0dd017b1a08f3bf0ccc644
Jeo D <e8gea6w5z8id@opayq.com>
2019-01-27 21:18:14 -0700

    Updates

---
+++ precache-manifest.c193767c1951e5e15e36edcfc3fc413d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/swapi-box/static/media/Starjhol.9c91297e.ttf"
+  },
+  {
+    "revision": "37aee6c8c1d16c287d66",
+    "url": "/swapi-box/static/js/runtime~main.37aee6c8.js"
+  },
+  {
+    "revision": "bb12ebafbf05de870caa",
+    "url": "/swapi-box/static/js/main.bb12ebaf.chunk.js"
+  },
+  {
+    "revision": "10231307add5b7881f3d",
+    "url": "/swapi-box/static/js/1.10231307.chunk.js"
+  },
+  {
+    "revision": "bb12ebafbf05de870caa",
+    "url": "/swapi-box/static/css/main.1ee915aa.chunk.css"
+  },
+  {
+    "revision": "2139dff0d6db1139b06b701b2d241162",
+    "url": "/swapi-box/index.html"
+  }
+];
